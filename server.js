d49a5da0cfb0521bdef4096b07eaed54555da714
GlowingEgg <qhinman@gmail.com>
2020-04-15 15:28:21 -0700

    Adds an api router and hot reloading.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+var reload = require('express-reload')
 const next = require('next');
+const apiRouter = require('./api');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -9,13 +11,23 @@ app.prepare()
 .then(() => {
   const server = express()
 
+  // the path to the api router file
+  const apiPath = `${__dirname}/api/`
+
+  // hot reloading of the api
+  server.use(reload(apiPath));
+
+  // attach the api router
+  server.use('/api', apiRouter);
+
+  // let next.js handle any unhandled routes (and serve the app)
   server.get('*', (req, res) => {
     return handle(req, res)
   })
 
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('👂 listening on 3000');
   })
 })
 .catch((ex) => {
